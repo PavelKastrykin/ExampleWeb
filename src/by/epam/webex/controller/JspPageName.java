2b0c414f9039e8466c2ef8c9b5f74c092634e41c
Pavel Kastrykin <offerent@gmail.com>
2015-03-20 23:09:27 +0200

    got it

--- src/by/epam/webex/controller/JspPageName.java
+++ src/by/epam/webex/controller/JspPageName.java
@@ -3,6 +3,6 @@ package by.epam.webex.controller;
 public class JspPageName {
     private JspPageName(){}
 
-    public static final String USER_PAGE = "jsp/userpage.jsp";
+    public static final String USER_PAGE = "jsp/userPage.jsp";
     public static final String ERROR_PAGE = "error.jsp";
 }
